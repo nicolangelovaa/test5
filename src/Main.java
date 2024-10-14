c5a6c16e05048e81e7fa05f0e0341db70d2094c3
Nicol <nicol.angelova.a68@learn.telerikacademy.com>
2024-10-14 13:30:03 +0300

    git init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Hello Git!");
+
+        System.out.println("Now I can continue make changes to my project :)");
     }
 }
